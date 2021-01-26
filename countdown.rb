11906be69aada2c5da3bd21ad15156248b721d94
Radele Rose <radele.rose@gmail.com>
2021-01-26 05:08:40 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,6 @@
 #write your code here
 
-def countdown(seconds_to_midnight) 
+def countdown(seconds_to_midnight)
   while seconds_to_midnight > 0
     puts "#{seconds_to_midnight} SECOND(S)!"
     seconds_to_midnight -= 1
